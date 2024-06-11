5c09766ee22e8b76ca0e22b0629e937dd0f2223b
ali <ibrakhimov7747a@gmail.com>
2024-06-11 17:02:47 +0900

    feat: publish homepage screen component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,20 @@
-import withLayoutMain from "@/libs/components/layout/LayoutHome";
+import TrendProperties from "../libs/components/homepage/TrendProperties";
+import PopularProperties from "../libs/components/homepage/PopularProperties";
+import Advertisement from "../libs/components/homepage/Advertisement";
+import TopProperties from "../libs/components/homepage/TopProperties";
+import TopAgents from "../libs/components/homepage/TopAgents";
+import withLayoutMain from "../libs/components/layout/LayoutHome";
 import { Stack } from "@mui/material";
 import { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
     <Stack className={"home-page"}>
-      <Stack>
-        <Stack className="container">Trend Properties</Stack>
-      </Stack>
-      <Stack>
-        <Stack className="container">Popular Properties</Stack>
-      </Stack>
-      <Stack>
-        <Stack className="container">Advertisement</Stack>
-      </Stack>
-      <Stack>
-        <Stack className="container">Top Properties</Stack>
-      </Stack>
-      <Stack>
-        <Stack className="container">Top Agents</Stack>
-      </Stack>
+      <TrendProperties />
+      <PopularProperties />
+      <Advertisement />
+      <TopProperties />
+      <TopAgents />
     </Stack>
   );
 };
