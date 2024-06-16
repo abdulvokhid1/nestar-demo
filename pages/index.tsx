75571f37dde4efec26594d8de67c86238c2f6325
ali <ibrakhimov7747a@gmail.com>
2024-06-17 04:54:15 +0900

    feat: apollo client connection

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,39 @@
-import TrendProperties from "../libs/components/homepage/TrendProperties";
-import PopularProperties from "../libs/components/homepage/PopularProperties";
-import Advertisement from "../libs/components/homepage/Advertisement";
-import TopProperties from "../libs/components/homepage/TopProperties";
-import TopAgents from "../libs/components/homepage/TopAgents";
-import withLayoutMain from "../libs/components/layout/LayoutHome";
+import TrendProperties from "@/libs/components/homepage/TrendProperties";
+import PopularProperties from "@/libs/components/homepage/PopularProperties";
+import Advertisement from "@/libs/components/homepage/Advertisement";
+import TopProperties from "@/libs/components/homepage/TopProperties";
+import TopAgents from "@/libs/components/homepage/TopAgents";
+import withLayoutMain from "@/libs/components/layout/LayoutHome";
 import { Stack } from "@mui/material";
 import { NextPage } from "next";
 import useDeviceDetect from "@/libs/hooks/useDeviceDetect";
+import { useQuery } from "@apollo/client";
+import { GET_PROPERTIES } from "@/apollo/user/query";
 
 const Home: NextPage = () => {
   const device = useDeviceDetect();
 
+  const {
+    loading: getPropertiesLoading,
+    data: getPropertiesData,
+    error: getPropertiesError,
+    refetch: getPropertiesRefetch,
+  } = useQuery(GET_PROPERTIES, {
+    fetchPolicy: "network-only",
+    variables: {
+      input: {
+        page: 1,
+        limit: 5,
+        sort: "createdAt",
+        direction: "DESC",
+        search: {},
+      },
+    },
+  });
+  console.log("getPropertiesData =>", getPropertiesData);
+
   if (device === "mobile") {
-    return <Stack>Homepage Mobile</Stack>;
+    return <Stack>HOMEPAGE MOBILE</Stack>;
   } else {
     return (
       <Stack className={"home-page"}>
